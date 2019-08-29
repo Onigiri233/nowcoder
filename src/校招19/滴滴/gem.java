a27d86835d0807e5add4336d028b93a07aab262f
PanHongTao <1398703720@qq.com>
2019-08-29 15:38:30 +0800

    宝石。前缀字符串变形

--- src/校招19/滴滴/gem.java
+++ src/校招19/滴滴/gem.java
@@ -1,7 +1,6 @@
 package 校招19.滴滴;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by fantuan on 2019/8/28.
@@ -37,40 +36,69 @@ public class gem {
 
 //        System.out.println(sa + "||" + sb);
         int gemNum = preCount(sa, sb);
+
+        ArrayList<Integer> arrayListA = new ArrayList<>();
+        ArrayList<Integer> arrayListB = new ArrayList<>();
+        for (int i = 0; i < sa.length(); i = i + 2) {
+            if (sb.contains(sa.charAt(i) + "")) {
+                //如果存在就找到它出现的位置,不存在就不存.
+                arrayListA.add(i);
+                arrayListB.add(sb.indexOf(sa.charAt(i) + ""));
+            }
+        }
+
+//        System.out.println(arrayListA);
+//        System.out.println(arrayListB);
+
 //        找到
-        for (int resultNum=gemNum;resultNum>0;resultNum--){
-            if (cost * resultNum + costCurr < total) {
+        for (int resultNum = gemNum; resultNum > 0; resultNum--) {
+            if (cost * resultNum + costCurr <= total) {
                 return resultNum;
-            }else {
-                sb.charAt(resultNum);
-                // TODO: 2019/8/28 cost的回溯判断 
-                costCurr+=0;
+            } else if (resultNum==1){
+                return 0;
+            }
+            else{
+                // TODO: 2019/8/28 cost的回溯判断
+                costCurr += Integer.parseInt(sa.charAt(resultNum * 2) + "");
+                int starA, starB, endA, endB;
+                starA = arrayListA.get(resultNum - 2);
+                endA = arrayListA.get(resultNum - 1);
+                starB = arrayListB.get(resultNum - 2);
+                endB = arrayListB.get(resultNum - 1);
+                for (int i = starA; i <=endA; i++) {
+                    costCurr+=Integer.parseInt(sa.charAt(i)+"");
+                }
+                for (int i = starB; i <=endB; i++) {
+                    costCurr+=Integer.parseInt(sb.charAt(i)+"");
+                }
             }
         }
         return gemNum;
     }
 
     private static int preCount(String sa, String sb) {
-        ArrayList<Integer> arrayList=new ArrayList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        HashMap<Integer, Integer> hashMap = new HashMap();
         for (int i = 0; i < sa.length(); i = i + 2) {
             if (sb.contains(sa.charAt(i) + "")) {
                 //如果存在就找到它出现的位置,不存在就不存.
                 arrayList.add(sb.indexOf(sa.charAt(i) + ""));
+                hashMap.put(i, sb.indexOf(sa.charAt(i) + ""));
             }
         }
 //        System.out.println(arrayList);
         //写一个返回最长增序子序列长度的函数,返回值即为宝石数
-        int n=arrayList.size();
+        int n = arrayList.size();
         int[] number = new int[n];
         int[] increSeq = new int[n];
         for (int i = 0; i < n; i++) {
-            number[i]=arrayList.get(i);
+            number[i] = arrayList.get(i);
             increSeq[i] = 1;
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(number[i]>number[j]&&increSeq[j]+1>increSeq[i])
-                    increSeq[i]=increSeq[j]+1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (number[i] > number[j] && increSeq[j] + 1 > increSeq[i])
+                    increSeq[i] = increSeq[j] + 1;
             }
         }
         int max = 0;
