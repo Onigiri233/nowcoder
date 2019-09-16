7a52ebcdbf6c6afbcf33ea455376c77fc19bc874
PanHongTao <1398703720@qq.com>
2019-09-16 23:21:21 +0800

    完善了算法

--- src/校招19/字节跳动/试炼.java
+++ src/校招19/字节跳动/试炼.java
@@ -9,34 +9,56 @@ import java.util.Scanner;
  */
 public class 试炼 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner( System.in );
         while (sc.hasNext()) {
             int n = sc.nextInt();
             int[] nNums = new int[n];
             for (int i = 0; i < n; i++) {
                 nNums[i] = sc.nextInt();
             }
-            System.out.println(faction(nNums));
-
+            System.out.println( faction( nNums ) );
         }
         sc.close();
     }
 
     private static int faction(int[] nNums) {
-//        List jilist=new ArrayList<>();
-//        List oulist=new ArrayList<>();
-        int ji=0;
-        int ou=0;
-        for (int i = 0; i < nNums.length; i++) {
-            if (i%2==0){
-//                oulist.add(nNums[i]);
-                ou+=nNums[i];
-            }else {
-//                jilist.add(nNums[i]);
-                ji+=nNums[i];
-            }
-        }
 
-        return ji>ou?ji:ou;
+        if (nNums.length % 2 == 0) {
+            int ji = 0;
+            int ou = 0;
+            for (int i = 0; i < nNums.length; i++) {
+                if (i % 2 == 0) {
+                    ou += nNums[i];
+                } else {
+                    ji += nNums[i];
+                }
+            }
+            return ji > ou ? ji : ou;
+        } else {
+            int ji = 0;
+            int ou = 0;
+            int a=nNums[0];
+            for (int i = 1; i < nNums.length; i++) {
+                if (i % 2 == 0) {
+                    ou += nNums[i];
+                } else {
+                    ji += nNums[i];
+                }
+            }
+            a+=ji < ou ? ji : ou;
+
+            ji = 0;
+            ou = 0;
+            int b=nNums[nNums.length-1];
+            for (int i = 0; i < nNums.length-1; i++) {
+                if (i % 2 == 0) {
+                    ou += nNums[i];
+                } else {
+                    ji += nNums[i];
+                }
+            }
+            b+=ji < ou ? ji : ou;
+            return a < b ? b : a;
+        }
     }
 }
